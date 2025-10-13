95e80e1d5f915914613120e8de77316531b05420
taromonroy <taromonroy2@gmail.com>
2025-10-12 22:23:34 -0300

    resubo tp 5

--- TP5/tp5.py
+++ TP5/tp5.py
@@ -1,49 +1,49 @@
 '''
 1- Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función
 range.
-
+'''
 lista = []
 
 for i in range(1,101):
     if i % 4 == 0:
         lista.append(i)
 print(lista)
-'''
+
 
 '''
 2- Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el
 penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
 indexing con números negativos!
-
+'''
 lista = [1,2,3,4,5]
 
 print(lista[-2])
-'''
+
 
 '''
 3-  Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por
 pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. Por
 ejemplo:
 lista_vacia = []
-
+'''
 lista = []
 
 for i in range(3):
     lista.append(input("ingrese una palabra: "))
 print(lista)
-'''
+
 
 '''
 4-Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”,
 respectivamente. Imprimir la lista resultante por pantalla. ¡Puedes hacerlo como se muestra
 en los videos o bien investigar cómo funciona el indexing con números negativos!
 animales = ["perro", "gato", "conejo", "pez"]
-
+'''
 animales = ["perro", "gato", "conejo", "pez"]
 animales[1] = "loro"
 animales[-1]= "oso"
 print(animales)
-'''
+
 
 '''
 5- Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza
@@ -55,34 +55,34 @@ print(numeros)
 '''
 6- Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
 pantalla los dos primeros.
-
+'''
 lista = []
 for i in range(10,31,5):
     lista.append(i)
 
 print(lista)
-'''
+
 
 '''
 7- Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores
 cualesquiera.
 autos = ["sedan", "polo", "suran", "gol"]
-
+'''
 autos = ["sedan", "polo", "suran", "gol"]
 autos[1] = "chevrolet"
 autos[2] = "ford"
 print(autos)
-'''
+
 
 '''
 8-Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append
 directamente. Imprimir la lista resultante por pantalla.
-
+'''
 dobles = []
 for i in range(5,16,5):
     dobles.append(i*2)
 print(dobles)
-'''
+
 
 '''
 9-  Dada la lista “compras”, cuyos elementos representan los productos comprados por
@@ -93,6 +93,12 @@ b) Reemplazar "fideos" por "tallarines" en la lista del segundo cliente.
 c) Eliminar "pan" de la lista del primer cliente.
 d) Imprimir la lista resultante por pantalla
 '''
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+compras[2].append("jugo")
+compras[1][1]="tallarines"
+compras[0].remove("pan")
+print(compras)
+
 
 '''
 10- Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos:
@@ -104,3 +110,5 @@ d) Imprimir la lista resultante por pantalla
 ● Posición lista_anidada[3]: False
 Imprimir la lista resultante por pantalla.
 '''
+lista_anidada = [15, True, [25.5,57.9,30.6], False]
+print(lista_anidada)
