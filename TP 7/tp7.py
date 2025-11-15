cd5e4d242cf4d01e22e27b6d4b4c6b6f4fd71b63
taromonroy <taromonroy2@gmail.com>
2025-11-15 13:45:28 -0300

    subo tp 6 corregido

---
+++ TP 7/tp7.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 1
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+
+print(precios_frutas)
+
+precios_frutas["Naranja"] = 1200
+precios_frutas["Manzana"] = 1500
+precios_frutas["Pera"] = 2300
+
+print(precios_frutas)
+
+"""
--- TP6/tp6.py
+++ TP6/tp6.py
@@ -1,16 +1,16 @@
 """
 EJERCICIO 1
-
+"""
 def imprimir_hola_mundo():
     print("Hola Mundo!")
 
 imprimir_hola_mundo()
 
-"""
+
 
 """
 EJERCICIO 2
-
+"""
 def saludar_usuario(nombre):
     return f"Hola {nombre}!"
 
@@ -18,10 +18,10 @@ nombre = input("Ingrese su nombre: ")
 saludo = saludar_usuario(nombre)
 print(saludo)
 
-"""
+
 """
 EJERCICIO 3
-
+"""
 def informacion_personal(nombre, apellido, edad, residencia):
     print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.")
 
@@ -32,10 +32,10 @@ residencia = input("Ingrese su lugar de residencia: ")
 
 informacion_personal(nombre, apellido, edad, residencia)
 
-"""
+
 """
 EJERCICIO 4
-
+"""
 def calcular_area_circulo(radio):
     return 3.14 * radio ** 2
 
@@ -49,11 +49,12 @@ perimetro = calcular_perimetro_circulo(radio)
 print(f"El área del círculo es: {area:.2f}")
 print(f"El perímetro del círculo es: {perimetro:.2f}")
 
-"""
+
 
 """
 EJERCICIO 5
 
+"""
 def segundos_a_horas(segundos):
     return segundos / 3600
 
@@ -62,9 +63,8 @@ horas = segundos_a_horas(segundos)
 print(f"{segundos} segundos equivalen a {horas:.2f} horas.")
 
 """
-"""
 EJERCICIO 6
-
+"""
 def tabla_multiplicar(numero):
     print(f"Tabla de multiplicar de {numero}:")
     for i in range(1, 11):
@@ -73,10 +73,10 @@ def tabla_multiplicar(numero):
 numero = int(input("Ingrese un número para mostrar su tabla de multiplicar: "))
 tabla_multiplicar(numero)
 
-"""
+
 """
 EJERCICIO 7
-
+"""
 def operaciones_basicas(a, b):
     suma = a + b
     resta = a - b
@@ -101,11 +101,11 @@ if division is not None:
 else:
     print("División: No se puede dividir por cero.")
 
-"""
+
 
 """
 EJERCICIO 8
-
+"""
 def calcular_imc(peso, altura):
     return peso / (altura ** 2)
 
@@ -115,10 +115,10 @@ altura = float(input("Ingrese su altura en metros: "))
 imc = calcular_imc(peso, altura)
 print(f"Tu índice de masa corporal (IMC) es: {imc:.2f}")
 
-"""
+
 """
 EJERCICIO 9
-
+"""
 def celsius_a_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
@@ -126,10 +126,10 @@ celsius = float(input("Ingrese la temperatura en grados Celsius: "))
 fahrenheit = celsius_a_fahrenheit(celsius)
 print(f"{celsius}°C equivalen a {fahrenheit:.2f}°F")
 
-"""
+
 """
 EJERCICIO 10
-
+"""
 def calcular_promedio(a, b, c):
     return (a + b + c) / 3
 
@@ -140,4 +140,3 @@ num3 = float(input("Ingrese el tercer número: "))
 promedio = calcular_promedio(num1, num2, num3)
 print(f"El promedio de {num1}, {num2} y {num3} es: {promedio:.2f}")
 
-"""
